6f220b06ef233574f7086c8bf0a1af1ccbfde928
ruvik606 <ruvik606@gmail.com>
2022-08-25 10:08:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* alert ('Hello'); */
+
+/* const result = confirm("Are yuo here?"); */
+/* 
+const answer = +prompt("Вам есть 18?", "18");
+console.log(answer + 2); */
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', "" );
+answers[1] = prompt('Как ваше фамиля?', "" );
+answers[2] = prompt('Сколько вам лет', "" );
+
+console.log(typeof((null)));
